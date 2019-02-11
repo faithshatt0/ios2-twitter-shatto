d2d7ab02bd73d9b2e252a6969d99de4b61ca05ef
Gilbert Curbelo <curb6178@m506-118-26444.local>
2019-02-11 15:02:24 -0800

    Downgraded version

--- twitter/AppDelegate.swift
+++ twitter/AppDelegate.swift
@@ -1,12 +1,13 @@
 //
 //  AppDelegate.swift
-//  twitter
+//  Twitter
 //
-//  Created by Gilbert Curbelo on 2/11/19.
-//  Copyright © 2019 Faith Shatto. All rights reserved.
+//  Created by Dan on 1/3/19.
+//  Copyright © 2019 Dan. All rights reserved.
 //
 
 import UIKit
+import BDBOAuth1Manager
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
@@ -40,7 +41,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        TwitterAPICaller.client?.handleOpenUrl(url: url)
+        return true
+    }
 
 }
 
